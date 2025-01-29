283a38064b8131d6d4518ef5a6882cd19cd4a326
justrhoto <otisub@gmail.com>
2025-01-29 09:35:12 -0600

    Numbers and tweaks

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -37,9 +37,13 @@ export default function Home() {
     <div className="flex justify-center items-center h-screen">
       <div className={`grid grid-rows-${puzzle.body[0].dimensions.width} h-2/3 aspect-square`}>
         {Array.from({ length: puzzle.body[0].dimensions.height }).map((_, i) => (
-          <div key={i} className="grid grid-cols-15">
+          <div key={i} className={`grid grid-cols-${puzzle.body[0].dimensions.height}`}>
             {Array.from({ length: puzzle.body[0].dimensions.width }).map((_, j) => (
-              <div className={`border border-gray-500 bg-${puzzle.body[0].cells[(i * 15) + j].answer ? 'white' : 'black'} aspect-square w-auto h-auto`} key={`${i}-${j}`}></div>
+              <div className={`border border-gray-500 ${puzzle.body[0].cells[(i * 15) + j].answer ? 'bg-white' : 'bg-black'} aspect-square w-auto h-auto`} key={`${i}-${j}`}>
+                <div className="text-[1.2vh] text-black">
+                  {puzzle.body[0].cells[(i * 15) + j].label}
+                </div>
+              </div>
             ))}
           </div>
         ))}
