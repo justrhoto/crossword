d290c57b099ff1c317f7ab4240b81e0b41f5c45e
justrhoto <otisub@gmail.com>
2025-01-29 23:07:18 -0600

    Answer templating

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -46,12 +46,15 @@ export default function Home() {
         {Array.from({ length: puzzle.body[0].dimensions.height }).map((_, i) => (
           <div key={i} className={`grid grid-cols-${puzzle.body[0].dimensions.height}`}>
             {Array.from({ length: puzzle.body[0].dimensions.width }).map((_, j) => (
-              <div className={`border border-gray-500 ${puzzle.body[0].cells[(i * 15) + j].answer ? 'bg-white' : 'bg-black'} aspect-square w-auto h-auto`} key={`${i}-${j}`}>
-                {puzzle.body[0].cells[(i * 15) + j].answer &&
-                  <div className="text-[1.2vh] text-black select-none">
+              <div className={`relative border border-gray-500 ${puzzle.body[0].cells[(i * 15) + j].answer ? 'bg-white' : 'bg-black'} aspect-square w-auto h-auto`} key={`${i}-${j}`}>
+                {puzzle.body[0].cells[(i * 15) + j].label &&
+                  <div className="absolute inset-0 text-xs text-black select-none">
                     {puzzle.body[0].cells[(i * 15) + j].label}
-                  </div>
-                }
+                  </div>}
+                {puzzle.body[0].cells[(i * 15) + j].answer &&
+                  <div className="absolute inset-0 flex justify-center items-center text-3xl text-black select-none">
+                    {puzzle.body[0].cells[(i * 15) + j].answer}
+                  </div>}
               </div>
             ))}
           </div>
